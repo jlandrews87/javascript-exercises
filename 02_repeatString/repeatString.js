8d86f5097ff31515ec6bc371c917f74109ba18a7
jlandrews87 <155691210+jlandrews87@users.noreply.github.com>
2024-02-07 16:15:47 -0500

    Complete Exercises 2 -3
    I completed exercises 2 and 3. Pretty straight-forward.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,10 @@
 const repeatString = function(word, times) {
-    if (times < 0) return "ERROR";
-    let string = "";
-    for (let i = 0; i < times; i++) {
-        string += word;
-    }
-    return string;
-};
-console.log(repeatString("Bill!",4));
-// Do not edit below this line
+if (times === 0) return "ERROR";
+let string = '';
+for (let i = 0; i < times; i++) {
+    string += word;
+} return string;
+
+}
+
 module.exports = repeatString;
